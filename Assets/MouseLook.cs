ac111627b0d7a4883a435b507acfb8131f2ad719
JPierro0513 <jmpierro0513@gmail.com>
2021-03-01 11:48:46 -0600

    Updated movement and look: TODO Fix Cursor Lock

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -7,16 +7,12 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity = 100f;
     public Transform playerBody;
     float xRotation = 0f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
 
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetKeyDown("l")) Cursor.lockState = CursorLockMode.Locked;
+
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
